ac4a2b615e6300972c5b73fb21654a35512b4588
Linah Imbari <linahmuhonjaimbari@gmail.com>
2022-11-11 00:48:48 +0300

    add user_destinations associations

--- app/models/destination.rb
+++ app/models/destination.rb
@@ -1,4 +1,6 @@
 class Destination < ActiveRecord::Base
     has_many :reviews
-    has_many :users, through: :reviews
+    has_many :user_destinations
+    has_many :users, through: :user_destinations
+    
 end
